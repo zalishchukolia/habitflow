7509b9be517a2b812e9e96e0786759c6e01c1a35
Olia Zalishchuk <zalishchukolia123@gmail.com>
2026-05-06 22:01:01 +0300

    Fix PostHog proxy headers

--- api/ingest/[...path].js
+++ api/ingest/[...path].js
@@ -4,15 +4,13 @@ export default async function handler(req) {
   const url = new URL(req.url)
   const path = url.pathname.replace('/api/ingest', '')
   const isStatic = path.startsWith('/static/')
-  const host = isStatic
-    ? 'https://us-assets.i.posthog.com'
-    : 'https://us.i.posthog.com'
-
-  const target = `${host}${path}${url.search}`
+  const host = isStatic ? 'us-assets.i.posthog.com' : 'us.i.posthog.com'
+  const target = `https://${host}${path}${url.search}`
 
   const headers = new Headers(req.headers)
-  headers.set('host', isStatic ? 'us-assets.i.posthog.com' : 'us.i.posthog.com')
-  headers.set('x-forwarded-for', req.headers.get('x-forwarded-for') || '')
+  headers.set('host', host)
+  headers.set('origin', `https://${host}`)
+  headers.set('referer', `https://${host}${path}`)
 
   return fetch(target, {
     method: req.method,
