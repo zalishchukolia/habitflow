878c87ba38bfb57b444d8a3442843b98202183d1
Olia Zalishchuk <zalishchukolia123@gmail.com>
2026-05-07 00:53:51 +0300

    feat: add Sentry error tracking

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
 import posthog from 'posthog-js'
 import { PostHogProvider } from '@posthog/react'
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+  dsn: 'https://360323fea97c63ef90d77648fd0371df@o4511345154523136.ingest.de.sentry.io/4511345160355920',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  environment: 'production',
+})
 
 posthog.init('phc_vsj2aobx6g5hdqhy5J9DbHiBV28G96dzQir7cn4qqBHj', {
   api_host: '/api/ph',
