ecd23127085aec36d2d431426a38bfcb8c792cf7
Olia Zalishchuk <zalishchukolia123@gmail.com>
2026-05-06 22:11:36 +0300

    Fix PostHog function proxy

--- api/ingest/[...path].js
+++ api/ingest/[...path].js
@@ -3,18 +3,24 @@ export const config = { runtime: 'edge' }
 export default async function handler(req) {
   const url = new URL(req.url)
   const path = url.pathname.replace('/api/ingest', '')
-  const isStatic = path.startsWith('/static/')
-  const host = isStatic ? 'us-assets.i.posthog.com' : 'us.i.posthog.com'
+
+  const isAssets = path.startsWith('/static/') || path.startsWith('/array/')
+  const host = isAssets ? 'us-assets.i.posthog.com' : 'us.i.posthog.com'
   const target = `https://${host}${path}${url.search}`
 
   const headers = new Headers(req.headers)
   headers.set('host', host)
-  headers.set('origin', `https://${host}`)
-  headers.set('referer', `https://${host}${path}`)
+
+  const xff = req.headers.get('x-forwarded-for')
+  if (xff) headers.set('x-forwarded-for', xff)
+
+  headers.delete('content-length')
+  headers.delete('cookie')
 
   return fetch(target, {
     method: req.method,
     headers,
     body: req.method === 'GET' || req.method === 'HEAD' ? undefined : req.body,
+    redirect: 'follow',
   })
 }
