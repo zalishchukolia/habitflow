0a3ea61c9a529e8bbf1aa2139fda0d35de427597
Olia Zalishchuk <zalishchukolia123@gmail.com>
2026-05-11 22:14:54 +0300

    fix proxy

--- api/ph/[...path].js
+++ api/ph/[...path].js
@@ -2,14 +2,13 @@ export const config = { runtime: 'edge' }
 
 export default async function handler(req) {
   const url = new URL(req.url)
-  const path = url.pathname.replace(/^\/api\/ph/, '') || '/'
+  const path = url.pathname.replace(/^\/api\/ph/, '').replace(/\/$/, '') || '/'
 
   const host = 'eu.i.posthog.com'
   const target = `https://${host}${path}${url.search}`
 
   const headers = new Headers()
   headers.set('host', host)
-  headers.set('content-type', 'application/json')
   headers.set('content-type', req.headers.get('content-type') || 'application/json')
 
   const xff = req.headers.get('x-forwarded-for')
