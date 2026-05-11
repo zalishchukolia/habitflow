dfb8d91bc74ce652d48de1bf8a679739056fbadb
Olia Zalishchuk <zalishchukolia123@gmail.com>
2026-05-11 21:37:47 +0300

    fix proxy host

--- api/ph/[...path].js
+++ api/ph/[...path].js
@@ -4,8 +4,7 @@ export default async function handler(req) {
   const url = new URL(req.url)
   const path = url.pathname.replace(/^\/api\/ph/, '') || '/'
 
-  const isAssets = path.startsWith('/static/') || path.startsWith('/array/')
-  const host = isAssets ? 'eu-assets.i.posthog.com' : 'eu.i.posthog.com'
+  const host = 'eu.i.posthog.com'
   const target = `https://${host}${path}${url.search}`
 
   const headers = new Headers()
